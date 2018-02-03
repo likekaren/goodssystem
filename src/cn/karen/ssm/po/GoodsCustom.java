90a1e26154d78ce85836a7842c939d57e35ffa7e
likekaren <1207696166@qq.com>
2018-02-03 16:30:08 +0800

    查询需要跳转页面

--- src/cn/karen/ssm/po/GoodsCustom.java
+++ src/cn/karen/ssm/po/GoodsCustom.java
@@ -2,5 +2,14 @@ package cn.karen.ssm.po;
 
 public class GoodsCustom extends Goods{
 
+	private GoodsCustom goodsCustom;
+
+	public GoodsCustom getGoodsCustom() {
+		return goodsCustom;
+	}
+
+	public void setGoodsCustom(GoodsCustom goodsCustom) {
+		this.goodsCustom = goodsCustom;
+	}
 	
 }
